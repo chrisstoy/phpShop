fdb13cfa84b7dd0b23dc7eec1c0490ce4a9720af
cstoy <chrisstoy@gmail.com>
2016-04-28 10:15:47 -0400

    fixed provisioning script so that it will correctly update the xdebug settings in php.ini

--- Vagrant.provision.sh
+++ Vagrant.provision.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 echo I am provisioning...
-
 
 sudo apt-get -y update
 sudo apt-get -y install php5-dev php-pear
@@ -12,13 +12,20 @@ XDEBUG=$(find / -name 'xdebug.so' 2> /dev/null)
 ZEND_LINE=zend_extension="$XDEBUG"
 PHP_INI=/etc/php5/apache2/php.ini
 
-echo Updating $PHP_INI
-
-echo ";XDebug debugger setup" >> $PHP_INI
-echo $ZEND_LINE >> $PHP_INI
-echo [XDebug] >> $PHP_INI
-echo xdebug.cli_color=1 >> $PHP_INI
-echo xdebug.remote_enable=1 >> $PHP_INI
+if grep -q -i xdebug $PHP_INI
+then
+	echo XDebug already configured in $PHP_INI	
+else
+	echo Updating $PHP_INI with XDebug settings...
+	echo >> $PHP_INI
+	echo ";XDebug debugger setup" >> $PHP_INI
+	echo $ZEND_LINE >> $PHP_INI
+	echo [xdebug] >> $PHP_INI
+	echo xdebug.cli_color=1 >> $PHP_INI
+	echo xdebug.remote_enable=1 >> $PHP_INI
+	echo xdebug.remote_connect_back=1 >> $PHP_INI
+	echo xdebug.remote_port=9000 >> $PHP_INI	
+fi
 
 date > /etc/vagrant_provisioned_at
 echo DONE
